1805916b6d76795f420e707f79052d68f27ae2fd
mattb555 <pooh555190@gmail.com>
2019-10-15 19:09:21 -0700

    Came up with theory on implementing save request screen

--- src/app/component/graph-editor/index.js
+++ src/app/component/graph-editor/index.js
@@ -30,6 +30,7 @@ class GraphEditor extends Component {
                 edges: [],
             },
             activeNode: null,
+            savePrompt: false
         };
     }
 
@@ -42,6 +43,9 @@ class GraphEditor extends Component {
 
     componentDidUpdate(previousProps, previousState) {
         const {match} = this.props;
+        if (previousState.model.id != match.params.id && !match.params.id) {
+            this.state.savePrompt = true;
+        }
         if(match.params.hasOwnProperty('id') && match.params.id && previousState.model.id != match.params.id) {
             this.loadModel(match.params.id);
         } else if (match.params.id === undefined && previousState.model.id != match.params.id) {
@@ -56,7 +60,7 @@ class GraphEditor extends Component {
                     nodes: [],
                     edges: [],
                 },
-                activeNode: null,
+                activeNode: null
             })
         }
     }
@@ -111,45 +115,59 @@ class GraphEditor extends Component {
         this.updateData(graph);
     };
 
+    clearPrompt() {
+        this.setState({
+            savePrompt: false
+        })
+    }
+
     render() {
-        console.log("I rerendered");
         const {message} = this.props;
-        return (
-            <Fragment>
-                {message()}
-                <Container>
-                    <CsvRow
-                        graph={this.state.graph}
-                        createGraphFromJson={this.createGraphFromJson}
-                    />
-                    <SaveGraphRow
-                        model={this.state.model} updateModel={this.updateModel}
-                    />
-                    <Row>
-                        <Column span={9} mSpan={8} sSpan={6}>
-                            <GraphViewVis data={this.state.data}/>
-                        </Column>
-                        <Column span={3} mSpan={4} sSpan={6}>
-                            <ConnectionList
-                                graph={this.state.graph}
-                                updateGraph={this.updateGraph}
-                                updateData={this.updateData}
-                            />
-                        </Column>
-                    </Row>
-                    <Row>
-                        <Column>
-                            <GraphBuilderWrapped
-                                id={this.state.id}
-                                graph={this.state.graph}
-                                model={this.state.model}
-                                updateGraph={this.updateGraph}
-                                updateData={this.updateData}
-                            />
-                        </Column>
-                    </Row>
-                </Container>
-            </Fragment>
+        var i;
+        if (this.state.savePrompt) {
+            i = <div>
+                <p>Would you like to save your work?</p>
+                <button>Yes</button>
+                <button onClick={() => this.clearPrompt()}>No</button>
+            </div>
+        } else {
+            i = <Fragment>
+            {message()}
+            <Container>
+                <CsvRow
+                    graph={this.state.graph}
+                    createGraphFromJson={this.createGraphFromJson}
+                />
+                <SaveGraphRow
+                    model={this.state.model} updateModel={this.updateModel}
+                />
+                <Row>
+                    <Column span={9} mSpan={8} sSpan={6}>
+                        <GraphViewVis data={this.state.data}/>
+                    </Column>
+                    <Column span={3} mSpan={4} sSpan={6}>
+                        <ConnectionList
+                            graph={this.state.graph}
+                            updateGraph={this.updateGraph}
+                            updateData={this.updateData}
+                        />
+                    </Column>
+                </Row>
+                <Row>
+                    <Column>
+                        <GraphBuilderWrapped
+                            id={this.state.id}
+                            graph={this.state.graph}
+                            model={this.state.model}
+                            updateGraph={this.updateGraph}
+                            updateData={this.updateData}
+                        />
+                    </Column>
+                </Row>
+            </Container>
+        </Fragment>
+        }
+        return ( i
         );
     }
 }
